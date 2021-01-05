d89ea7569d9e46545aabace1e6cef58c18f2245b
rendramakmur <rendra1795@gmail.com>
2021-01-05 20:50:37 +0700

    Finish laundry day

---
+++ week1/wednesday/laundry_day/laundry_day.js
@@ -0,0 +1,17 @@
+/**
+ * Algoritma:
+ * 1. Ketahui berapa banyak pakaian yang ingin dicuci, dalam kasus ini yaitu 20.
+ * 2. Masukkan pakaian satu persatu sampai semua baju masuk ke mesin cuci.
+ * 3. Tombol power akan menyala jika semua pakaian sudah masuk ke mesin cuci.
+ * 
+ * Pseudocode:
+ * STORE pakaian WITH NUMBER 20
+ * FOR i FROM 0 TO pakaian INCREMENT BY 1
+ *      DISPLAY "Pakaian ke " CONCAT i CONCAT " sudah masuk ke dalam mesin cuci."
+ *      IF i EQUALS TO pakian
+ *          DISPLAY "Mesin dinyalakan."
+ *      END IF
+ * END FOR
+ * 
+ * 
+ */
