f243e9ec4fd55a7450920a9723a246e9ba0621f8
rendramakmur <rendra1795@gmail.com>
2021-01-05 17:40:08 +0700

    Folder untuk practice

---
+++ practice/practice.js
@@ -0,0 +1,20 @@
+var nilai = undefined;
+
+// Jika nilai falsy tampilkan "Masih salah gan"
+// Jika nilai truthy tampilkan "Yeay aku dapet ferrari"
+
+
+// 1.
+if(!nilai) {
+    console.log('Masih salah gan');
+} else {
+    console.log('Yeay aku dapet ferrari');
+}
+
+
+// 2.
+if(nilai) {
+    console.log('Yeay aku dapet ferrari')
+} else {
+    console.log('Masih salah gan')
+}
