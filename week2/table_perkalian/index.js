bed9187c53ae5944636eda8a18049ccc7cbc9216
rendramakmur <rendra1795@gmail.com>
2021-01-12 22:22:12 +0700

    Table perkalian done.

---
+++ week2/table_perkalian/index.js
@@ -0,0 +1,28 @@
+function tabelPerkalian (angka) {
+    let perkalian = [];
+    let table = [];
+
+    for (let i = 0; i <= angka; i++) {
+        if (angka % i === 0) {
+            perkalian.push(i);
+        }
+    }
+    // console.log(perkalian);
+
+    for (let j = 0; j < perkalian.length; j++) {
+        if (j === perkalian.length - j) {
+            break;
+        } else {
+            table.push(`${perkalian[j]}x${perkalian[perkalian.length - 1 - j]}`)
+        }
+    }
+    // console.log(table);
+    return table;
+}
+
+// TEST CASES
+console.log(tabelPerkalian(24)); // [ '1x24', '2x12', '3x8', '4x6' ]
+console.log(tabelPerkalian(90)); // [ '1x90', '2x45', '3x30', '5x18', '6x15', '9x10' ]
+console.log(tabelPerkalian(20)); // [ '1x20', '2x10', '4x5']
+console.log(tabelPerkalian(179)); // [ '1x179' ]
+console.log(tabelPerkalian(1)); // [ '1x1' ]
