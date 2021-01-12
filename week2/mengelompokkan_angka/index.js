9b640b52547937e8630e5a5aec58c2e06628d366
rendramakmur <rendra1795@gmail.com>
2021-01-12 22:10:38 +0700

    Mengelompokkan kata done.

---
+++ week2/mengelompokkan_angka/index.js
@@ -0,0 +1,26 @@
+function mengelompokkanAngka (arr) {
+    let tampung = [];
+    let ganjil = [];
+    let genap = [];
+    let kelipatanTiga = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 !== 0 && arr[i] % 3 !== 0) {
+            ganjil.push(arr[i]);
+        } else if (arr[i] % 2 === 0 && arr[i] % 3 !== 0) {
+            genap.push(arr[i])
+        } else if (arr[i] % 3 === 0) {
+            kelipatanTiga.push(arr[i])
+        }
+    }
+
+    tampung.push(genap, ganjil, kelipatanTiga);
+
+    return tampung;
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
