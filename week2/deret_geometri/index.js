fa6cae60ab246485052cc1c24e94dd775794871a
rendramakmur <rendra1795@gmail.com>
2021-01-12 20:52:04 +0700

    Deret geometri done

---
+++ week2/deret_geometri/index.js
@@ -0,0 +1,21 @@
+function tentukanDeretGeometri (arr) {
+    let multiple = arr[1]/arr[0];
+    let geometri = false;
+
+    for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] * multiple === arr[i + 1]) {
+            geometri = true;
+            // console.log(arr[i] + ' ' + arr [i + 1] + ' ' + geometri);
+        } else if (arr[i] * multiple !== arr[i + 1]) {
+            geometri = false;
+        }
+    }
+    return geometri;
+}
+
+// TEST CASES
+console.log(tentukanDeretGeometri([1, 3, 9, 27, 81])); // true
+console.log(tentukanDeretGeometri([2, 4, 8, 16, 32])); // true
+console.log(tentukanDeretGeometri([2, 4, 6, 8])); // false
+console.log(tentukanDeretGeometri([2, 6, 18, 54])); // true
+console.log(tentukanDeretGeometri([1, 2, 3, 4, 7, 9])); // false
