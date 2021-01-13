eaee7b95f5e0b33acbee27f1d49a1b5f8c254c4f
rendramakmur <rendra1795@gmail.com>
2021-01-13 21:05:26 +0700

    Password generator done.

---
+++ week2/password_generator/index.js
@@ -0,0 +1,106 @@
+/*
+a -> b
+i -> j
+u -> v
+e -> f
+o -> p
+A -> B
+I -> J
+U -> V
+E -> F
+O -> P
+*/
+// Change vocalsnya
+
+function changeVocals (str) {
+    let vocals = 'aiueoAIUEO';
+    let replacement = 'bjvfpBJVFP';
+    let output = '';
+
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j <= vocals.length; j++) {
+            if (str[i] === vocals[j]) {
+                output += replacement[j];
+                break;
+            } else if (vocals[j] === undefined) {
+                output += str[i];
+            }
+        }
+    }
+
+    return output;
+
+}
+
+// console.log(changeVocals('Alexei'));
+
+// Reverse katanya
+function reverseWord (str) {
+    let output = '';
+
+    for (let i = str.length - 1; i >= 0; i--) {
+        output += str[i];
+    }
+
+    return output;
+}
+
+// console.log(reverseWord('Alexei'));
+
+// Ubah huruf besar ke kecil, kecil ke besar.
+function setLowerUpperCase (str) {
+    let lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+    let upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let output = '';
+    
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j <= lowerCase.length; j++) {
+            if (str[i] === lowerCase[j]) {
+                output += upperCase[j];
+            } else if (str[i] === upperCase[j]) {
+                output += lowerCase[j];
+            }
+        }
+    }
+
+    return output;
+    
+}
+
+// console.log(setLowerUpperCase('Alexei'));
+
+// Hilangkan semua spasi
+function removeSpace (str) {
+    let output = '';
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] !== ' ') {
+            output += str[i]
+        }
+    }
+
+    return output;
+}
+
+// console.log(removeSpace('Sergei Dragunov'));
+
+// Gunakan semua function diatas di dalam function passGenerator
+function passwordGenerator (name) {
+    if (name.length <= 5) {
+        return 'Minimal karakter yang diinput adalah 5 karakter'
+    }
+
+    let output1 = changeVocals(name);
+    let output2 = reverseWord(output1);
+    let output3 = setLowerUpperCase(output2);
+    let output4 = removeSpace(output3);
+
+    return output4;
+
+}
+
+// TEST CASE
+console.log(passwordGenerator('Sergei Dragunov')); // 'VPNVGBRdJFGRFs'
+console.log(passwordGenerator('Dimitri Wahyudiputra')); // 'BRTVPJDVYHBwJRTJMJd'
+console.log(passwordGenerator('Alexei')); // 'JFXFLb'
+console.log(passwordGenerator('Alex')); // 'Minimal karakter yang diinputkan adalah 5 karakter'
