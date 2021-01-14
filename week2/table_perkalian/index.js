2abae4cb8ae7c98a8e941ec5b19fcf1dcd1c94d9
rendramakmur <rendra1795@gmail.com>
2021-01-14 19:22:50 +0700

    Shorter way

--- week2/table_perkalian/index.js
+++ week2/table_perkalian/index.js
@@ -1,24 +1,43 @@
-function tabelPerkalian (angka) {
-    let perkalian = [];
-    let table = [];
+// function tabelPerkalian (angka) {
+//     let perkalian = [];
+//     let table = [];
 
-    for (let i = 0; i <= angka; i++) {
-        if (angka % i === 0) {
-            perkalian.push(i);
-        }
-    }
-    // console.log(perkalian);
+//     for (let i = 0; i <= angka; i++) {
+//         if (angka % i === 0) {
+//             perkalian.push(i);
+//         }
+//     }
+//     console.log(perkalian);
 
-    for (let j = 0; j < perkalian.length; j++) {
-        if (j === perkalian.length - j) {
-            break;
-        } else {
-            table.push(`${perkalian[j]}x${perkalian[perkalian.length - 1 - j]}`)
-        }
+//     for (let j = 0; j < perkalian.length; j++) {
+//         if (j === perkalian.length - j) {
+//             break;
+//         } else {
+//             table.push(`${perkalian[j]}x${perkalian[perkalian.length - 1 - j]}`)
+//         }
+//     }
+//     // console.log(table);
+//     return table;
+// }
+
+// SHORTER WAY
+
+function tabelPerkalian(angka) {
+    // you can only write your code here!
+    let result = [];
+    for (let i = 1; i <= angka; i++) {
+      // kalau i udah lebih besar daripada hasil baginya, maka di break
+      if (i > angka/i) {
+        break;
+      }
+      if (angka % i === 0) {
+        result.push(`${i}x${angka/i}`)
+      }
     }
-    // console.log(table);
-    return table;
-}
+  
+    return result;
+  }
+  
 
 // TEST CASES
 console.log(tabelPerkalian(24)); // [ '1x24', '2x12', '3x8', '4x6' ]
