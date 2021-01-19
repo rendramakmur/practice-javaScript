e5d44b99c8331c471ce1659a6016b7d818f5ef3e
rendramakmur <rendra1795@gmail.com>
2021-01-19 21:20:50 +0700

    Latihan array bangku kosong array

--- practice/latihan_array.js
+++ practice/latihan_array.js
@@ -1,3 +1,5 @@
+// Nomor 1
+
 /*
 ===========================
 Tambah penumpang Bus
@@ -100,3 +102,94 @@ function tambahPenumpangBus(arr1, arr2) {
   output :
   'Miya, Zilong, Layla, dan Grock tidak mendapatkan kursi bus. Silahkan cari bus lainnya'
   */
+
+
+// Nomor 2
+
+/*
+===================
+Coordinate Tracker
+===================
+
+[INSTRUCTIONS]
+coordinateTracker adalah sebuah function yang menerima dua parameter berupa array of array dan string.
+parameter string yang diterima, hanya menerima 2 yaitu:
+- 'ganjil' : maka akan mencari angka ganjil
+- 'genap' : maka akan mencari angka genap
+- 'x' : maka akan mencari 'x'
+
+function mencari nilai terbanyak dari parameter yang diinput, dan akan mereturn sebuah string seperti:
+'<search> yang paling banyak ada di baris ke-<index>'
+
+PS: Baris di mulai dari 1 yang paling awal, sampai baris ke akhir
+
+[RULE]
+- Hanya boleh menggunakan sintaks for/while, if-else, serta operasi array untuk pemecahan masalah.
+- Dilarang menggunakan sintaks .split()! Jangan lupa, split bukanlah operasi array.
+- Dilarang menggunakan regex .match dan lainnya!
+
+[EXAMPLE]
+input: ([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+], 'genap)
+proses: akan mencari nulai genap paling banyak, dan akan menyimpan baris mana yang terdapat bilangan genap
+output: 'genap yang paling banyak ada di baris ke-2'
+*/
+
+function cekGanjil(bilangan){
+  let output = 0;
+
+  if (bilangan % 2 !== 0) {
+    output++;
+  }
+
+  return output;
+}
+
+// cekGanjil();
+
+function cekGenap(bilangan){
+  let output = 0;
+
+  if (bilangan % 2 === 0) {
+    output++;
+  }
+
+  return output;
+}
+
+// cekGenap();
+
+function cekX(string){
+  
+}
+
+function getBanyak(array, checker_function){
+  
+}
+
+function coordinateTracker(arrayOfArray, search) {
+  // Code here
+ 
+}
+
+console.log(coordinateTracker([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9]
+], 'genap')); // 'genap yang paling banyak ada di baris ke-2'
+
+console.log(coordinateTracker([
+  [2, 2, 7],
+  [1, 3, 5, 6],
+  [1, 3, 5, 7, 9, 11]
+], 'ganjil')); // 'ganjil yang paling banyak ada di baris ke-3'
+
+console.log(coordinateTracker([
+  ['o', 'o', 'o', 'x'],
+  ['x', 'x', 'o'],
+  ['o', 'x'],
+  ['x', 'x', 'x', 'x', 'x', 'x', 'x']
+], 'x')); // 'x yang paling banyak ada di baris ke-4'
